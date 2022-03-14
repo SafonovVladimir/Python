7103c7fcdb1b7308783a5bbcb3eb51e115b96b99
SafonovVladimir <vladimir_safonov@ukr.net>
2022-03-14 15:00:46 +0200

    Merge remote-tracking branch 'origin/master'

--- leson14.py
+++ leson14.py
@@ -1,5 +1,10 @@
-lst1 = list(str(123))
-l2 = [i for i in 'hello']
-l3 = [i for i in 'hello world!' if i !=' ' and i not in ['a', 'e', 'i', 'y', 'o']]
-print(lst1, l2, l3, sep='\n')
+# lst1 = list(str(123))
+# l2 = [i for i in 'hello']
+# l3 = [i for i in 'hello world!' if i !=' ' and i not in ['a', 'e', 'i', 'y', 'o']]
+# l6 = list(range(1, 11))
+# print(lst1, l2, l3, l6, sep='\n')
 
+for i in range(1, 5):
+    print(f'Внешньій цикл №{i}')
+    for j in range(1, 5):
+        print(f'\tВнутренний цикл №{j}')
