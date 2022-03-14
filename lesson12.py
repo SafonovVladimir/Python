df830b1f9e822bcb9e6df3ae4f97ac5fc3c8fc6c
SafonovVladimir <vladimir_safonov@ukr.net>
2022-03-14 07:32:26 +0200

    Lesson 12

--- lesson12.py
+++ lesson12.py
@@ -1,6 +1,10 @@
-age = int(input('Сколько Вам лет? '))
+prices = {'apple': 0.75, 'egg': 0.50}
+cart = {
+  'apple': 1,
+  'egg': 6
+}
 
-if age >= 18:
-    print('Добро пожаловать!')
-else:
-    print(f'Вам {age} лет. Вы еще слишком юн, мой дорогой друг! Вам не хватает {18 - age} лет')
+bill = sum(prices[item] * cart[item]
+           for item in cart)
+
+print(f'I have to pay {bill:.2f}')
