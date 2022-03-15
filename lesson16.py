c1fb663acfc9cb134ed4784443b6e1bf0b603298
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-15 12:56:51 +0200

    Новий урок

--- lesson16.py
+++ lesson16.py
@@ -1,4 +1,11 @@
 lst = [1, 2, 3, 'hello', ['test', 10], 'world', True]
+lst2 = [4, 6, 'rest']
 lenth = len(lst)
-print(lst[0])
-print(lenth)
+print(lst[4][0])
+print(lst[0:3])
+lst[2] = 5
+lst.extend(lst2)
+lst.insert(1, 'world')
+lst.remove('world')
+print(lst.pop(1))
+print(lst)
