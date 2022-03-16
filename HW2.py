a1b961e0abeeeedfe72894e602c657d65996cea4
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-16 13:46:22 +0200

    HW2

---
+++ HW2.py
@@ -0,0 +1,34 @@
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # l2 = []
+# # i = 0
+# # while i < len(l1):
+# #     l2.append(l1[i] * 2)
+# #     i += 1
+l2 = [i * 2 for i in l1]
+print(l2)
+
+#
+# l1 = [1, 2, 3]
+# l2 = []
+# i = 0
+# k = 0
+# while i < len(l1):
+#     l2.append(l1[i] ** 2)
+#     print(str(l1[i]) + '^2', end=' ')
+#     k = k + l2[i]
+#     i += 1
+# print('\n', k)
+# res = 0
+# for num in l1:
+#     res += num ** 2
+# print(res)
+
+# time = float(input('Введите время: '))
+# print('litres = ' + str(int(time / 2)))
+
+# s = 'Hello world!'
+# if ' ' in s:
+#     s = s.upper()
+# else:
+#     s = s.lower()
+# print(s)
--- lesson17.py
+++ lesson17.py
@@ -2,3 +2,5 @@ x = 10
 print(x, id(x))
 x = 20
 print(x, id(x))
+x = 30
+print(x, id(x))
