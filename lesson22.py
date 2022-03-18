5adcde95c542d3fcafd5a15c8c3af9560195a3d8
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-18 09:47:06 +0200

    new lessons

---
+++ lesson22.py
@@ -0,0 +1,14 @@
+s1 = ('apple', 'orange', 'banana', 'apple', 'pear', 'orange')
+s2 = set('hello')
+s3 = {i for i in range(1, 10)}
+s4 = {1, 10, 4, 8, 2, 5}
+
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(type(s4))
+
+nums = [1, 2, 3, 4, 3, 2, 5]
+nums2 = set(nums)
+print(nums2)
