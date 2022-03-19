1f6a31ff5b2af0993512382676ebdae2a4f61318
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-19 15:45:38 +0200

    HW7

--- HW7.py
+++ HW7.py
@@ -15,12 +15,31 @@
 # print(odd_ball([1, 2, 3, 'even', 4, 5, 8, 7, 'odd']))
 # print(odd_ball([1, 2, 3, 'even', 4, 5, 44, 7, 9,'odd']))
 
-def find_sum(x):
-    for value in range(1, x+1):
-        if value % 3 == 0:
-            print(value)
-        else:
-            continue
-    # return a
+# def find_sum(x):
+#     lst = []
+#     for value in range(1, x + 1):
+#         if (value % 3 == 0) or (value % 5 == 0):
+#             lst.append(value)
+#         else:
+#             continue
+#     total = sum(lst)
+#     return total
 
-find_sum(6)
+# def find_sum(x):
+#     return sum([i for i in range(1, x + 1) if (i % 3 == 0) or (i % 5 == 0)])
+#
+# print(find_sum(5))
+# print(find_sum(10))
+names = ['Ryan', 'John', 'David', 'Jessika', 'Vova', 'sdf', 'asdf', 'asfasdf']
+
+def get_name_with_4_letters(names):
+    # lst = []
+    # for name in names:
+    #     # print(name)
+    #     if len(name) == 4:
+    #         lst.append(name)
+    # return lst
+    return ([name for name in names if len(name) == 4])
+
+print(get_name_with_4_letters(names))
+# get_name_with_4_letters(names)
