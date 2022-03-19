d5f12943c71afbe340f29900a88f6be8b9790e7f
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-19 14:32:46 +0200

    HW7

--- HW7.py
+++ HW7.py
@@ -1,16 +1,26 @@
 # arr = [1, 2, 3, 'even', 4, 5, 44, 7, 'odd']
 
 
-def odd_ball(arr):
-    global x
-    for i in arr:
-        if arr.__contains__(arr.index('odd')):
-            x = 'True'
+# def odd_ball(arr):
+#     global x
+#     for i in arr:
+#         if arr.__contains__(arr.index('odd')):
+#             x = 'True'
+#         else:
+#             x = 'False'
+#     return x
+#
+#
+# print(odd_ball([1, 2, 3, 'even', 4, 5, 44, 7, 'odd']))
+# print(odd_ball([1, 2, 3, 'even', 4, 5, 8, 7, 'odd']))
+# print(odd_ball([1, 2, 3, 'even', 4, 5, 44, 7, 9,'odd']))
+
+def find_sum(x):
+    for value in range(1, x+1):
+        if value % 3 == 0:
+            print(value)
         else:
-            x = 'False'
-    return x
+            continue
+    # return a
 
-
-print(odd_ball([1, 2, 3, 'even', 4, 5, 44, 7, 'odd']))
-print(odd_ball([1, 2, 3, 'even', 4, 5, 8, 7, 'odd']))
-print(odd_ball([1, 2, 3, 'even', 4, 5, 44, 7, 9,'odd']))
+find_sum(6)
