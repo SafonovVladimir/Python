72a24a5322631b272d336b178dc7bf36212f5d06
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-19 12:23:14 +0200

    lesson28

---
+++ lesson28.py
@@ -0,0 +1,33 @@
+# def get_sum(a, b):
+#     """
+#     Возвращает сумму аргументов а и b.
+#
+#     """
+#     return a + b
+#
+#
+# print(get_sum(1, 2))
+a = 5
+# def f():
+#     a = 10
+#     a += 1
+#     print(id(a))
+# print(id(a))
+# f()
+# print(id(a))
+
+# def f():
+#     global a
+#     a += 1
+#
+# f()
+# print(a)
+
+l = [1, 2, 3, 4, 5]
+# print([i * 2 for i in l])
+
+def f(l):
+    return [i * 2 for i in l]
+
+print(f(l))
+
