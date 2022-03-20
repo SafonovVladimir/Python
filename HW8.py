ad0823326a96c838282081cc18f63da57385521b
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-20 11:17:04 +0200

    HW4 new version

--- HW8.py
+++ HW8.py
@@ -1,7 +1,16 @@
 import fsworks
-import os
+# import os
 
 # os_path = os.getcwd()
 # print(os_path)
 
-fsworks.create_folder()
+case = input('Do you want to make a Directory or File, or building a DirTree?\n'
+             '(please enter "D" for Folder, "F" for file, "T" for Tree)\n')
+if case == 'D':
+    fsworks.create_dir()
+elif case == 'F':
+    fsworks.create_file()
+elif case == 'T':
+    fsworks.show_foder_tree()
+else:
+    print('Thank you!')
--- fsworks.py
+++ fsworks.py
@@ -1,35 +1,46 @@
-import  os
+import os
 
 
-def create_folder():
-    #Enter dir
+def create_dir():
+    # Enter dir
     path_parent_folder = input('Enter the path for creating folder: ')
     # os.chdir(path_parent_folder)
 
     # Enter the Dir name
     folder_name = input('Enter the Folder name: ')
 
-    #Path
+    # Path
     path = os.path.join(path_parent_folder, folder_name)
 
     # newpath = fr'{path_parent_folder}"\{folder_name}"'
     if not os.path.exists(path):
-        os.makedir(path)
+        os.makedirs(path)
     print(f'Folder {folder_name} is created in PATH {path_parent_folder}')
-    return path
-
+    # return path
 
 
 def create_file():
-    return path
+    # Enter the Path
+    path_parent_folder = input('Enter the path for creating folder: ')
+
+    # Enter File Name
+    filename = input('Enter the File name: ')
+
+    # Create file with name == file_name
+    file = open(os.path.join(path_parent_folder, filename), 'x')
+
+    print(f'File with name {filename} is created in PATH {path_parent_folder}')
+    # return filename
 
 
 def show_foder_tree():
-    folder_tree = os.listdir(input('Enter the path for watching folder tree: '))
-    return folder_tree
-
-
-
-
-
-
+    #Водим стартовый путь
+    startpath = input('Enter the path for watching folder tree: ')
+    for root, dirs, files in os.walk(startpath):
+        #считаем количество директорий и файлов
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
