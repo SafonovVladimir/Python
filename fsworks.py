99c530ab859db8095f4e58f0f8488e93d9d250b5
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-20 09:50:53 +0200

    HW4 new version

---
+++ fsworks.py
@@ -0,0 +1,18 @@
+def create_folder():
+    print(input('Enter the path for creating folder: '))
+
+    return path
+
+
+def create_file():
+    return path
+
+
+def show_tree():
+    return show
+
+
+
+
+
+
