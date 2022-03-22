59646addc6eeb4c03fc9f97d2a5043577f932004
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-22 21:22:19 +0200

    Lesson39

--- Parser_class.py
+++ Parser_class.py
@@ -32,11 +32,11 @@ class Parser:
             })
 
     def save(self):
-        with open (self.path, 'w', encoding='utf-8') as f:
+        with open(self.path, 'w', encoding='utf-8') as f:
             i = 1
             for item in self.results:
                 f.write(f'Новость №{i}\n\nНазвание: {item["title"]}\nДата новости: {item["news_time"]}\n'
-                        f'Ссылка: https://www.f1news.ru{item["link"]}\n\n******************************************\n\n')
+                        f'Ссылка: https://www.f1news.ru{item["link"]}\n\n**********************************************\n\n')
                 i += 1
             f.close()
 
@@ -44,3 +44,4 @@ class Parser:
         self.get_html()
         self.parsing()
         self.save()
+        print('Done')
--- lesson39.py
+++ lesson39.py
@@ -2,5 +2,3 @@ from Parser_class import Parser
 
 parser = Parser('https://www.f1news.ru/', 'f-1news.txt')
 parser.run()
-# print(parser.raw_html)
-# print(parser.html)
