e7bb236af1527ddd4cfc1958aa2b908a1610d49e
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-22 13:57:09 +0200

    Lesson37

--- classes.py
+++ classes.py
@@ -1,2 +1,8 @@
-class Person:
-    pass
+class Person():
+    name = 'John'
+
+    def __init__(self, name):
+        self.name = name
+
+    def print_info(self):
+        print(f'Hello. My name is {self.name}')
---
+++ lesson37.py
@@ -0,0 +1,7 @@
+from classes import Person
+
+person1 = Person()
+person1.print_info()
+person2 = Person()
+print(person2.name)
+
