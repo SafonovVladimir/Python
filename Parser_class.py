00075418ad8d7c13875a448f40c391b40151bbeb
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-22 20:37:03 +0200

    Lesson39

---
+++ Parser_class.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+class Parser:
+    raw_html = ''
+    html = ''
+    results = []
+
+    def __init__(self, url, path):
+        self.url = url
+        self.path = path
+
+    def get_html(self):
+        req = urllib.request.urlopen(self.url)
+        self.raw_html = req.read()
+        self.html = BeautifulSoup(self.raw_html, "html.parser")
+
+    def parsing(self):
+        news = self.html.find_all('li', class_='b-news-list__item')
+        for item in news:
+            title = item.find('a', class_='b-news-list__title').get_text()
+            # print(title)
+            link = item.a.get('href')
+            # print(link)
+            news_date = item.find('a', class_='b-news-list__date').get_text()
+            # print(news_date)
+            self.results.append({
+                'title': title,
+                'link': link,
+                'news_time': news_date
+            })
+
+    def save(self):
+        with open (self.path, 'w', encoding='utf-8') as f:
+            i = 1
+            for item in self.results:
+                f.write(f'Новость №{i}\n\nНазвание: {item["title"]}\nДата новости: {item["news_time"]}\n'
+                        f'Ссылка: https://www.f1news.ru{item["link"]}\n\n******************************************\n\n')
+                i += 1
+            f.close()
+
+    def run(self):
+        self.get_html()
+        self.parsing()
+        self.save()
--- lesson39.py
+++ lesson39.py
@@ -1,34 +1,6 @@
-from bs4 import BeautifulSoup
-# import request
-import urllib.request
+from Parser_class import Parser
 
-req = urllib.request.urlopen('https://www.f1news.ru/')
-html = req.read()
-# request.link
-# print(request.link)
-
-soap = BeautifulSoup(html, "html.parser")
-news = soap.find_all('li', class_='b-news-list__item')
-
-results = []
-for item in news:
-    title = item.find('a', class_='b-news-list__title').get_text()
-    # print(title)
-    link = item.a.get('href')
-    # print(link)
-    news_date = item.find('a', class_='b-news-list__date').get_text()
-    # print(news_date)
-    results.append({
-        'title': title,
-        'link': link,
-        'news_time': news_date
-    })
-f = open('f-1news.txt', 'w', encoding='utf-8')
-i = 1
-for item in results:
-    f.write(f'Новость №{i}\n\nНазвание: {item["title"]}\nДата новости: {item["news_time"]}\n'
-            f'Ссылка: https://www.f1news.ru{item["link"]}\n\n******************************************\n\n')
-    i += 1
-f.close()
-print('Done')
-# print(f'{results}\n')
+parser = Parser('https://www.f1news.ru/', 'f-1news.txt')
+parser.run()
+# print(parser.raw_html)
+# print(parser.html)
