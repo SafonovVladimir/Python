ae827cadf4f981ec848bc8c92b886bee5c48d7cb
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-24 17:48:11 +0200

    GUI

---
+++ GUI/lesson1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title('My first GUI APP')
+root.iconbitmap('python.ico')
+root.geometry('600x400+400+150')
+root.resizable(False, False)
+
+root.mainloop()
