41b06dc1cad9d295d2157df3e456fb71b4d7a3ee
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-24 12:48:05 +0200

    lesson47

--- new/lesson47.py
+++ new/lesson47.py
@@ -1,14 +1,15 @@
 import sqlite3
 
 
-def dict_factory(cursor, row):
+def dict_factory(cur, row):
     d = {}
-    for idx, col in enumerate(cursor.description):
+    for idx, col in enumerate(cur.description):
         d[col[0]] = row[idx]
     return d
 
 
 db = sqlite3.connect('test_db.db')
+db.row_factory = dict_factory
 cursor = db.cursor()
 
 email = 'petroff@gmail.com'
@@ -18,10 +19,19 @@ name = 'Pushin Vasa'
 # db.commit()
 # cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
 # cursor.execute("SELECT * FROM users WHERE email = ? OR name = ?", (email, name))
+# cursor.execute("SELECT * FROM users")
+# res = cursor.fetchall()
+# for user in res:
+#     print(user['name'], user['email'])
+#
+# print('********************')
+
+cursor.execute("DELETE FROM users WHERE email = ?", ('petroff@gmail.com',))
+db.commit()
+
 cursor.execute("SELECT * FROM users")
-
 res = cursor.fetchall()
-
-print(res)
+for user in res:
+    print(user['name'], user['email'])
 
 db.close()
