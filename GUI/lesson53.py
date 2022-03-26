51b22bbdf55bca266311744097e25104ec59587e
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-26 22:04:46 +0200

    lesson57

--- GUI/lesson53.py
+++ GUI/lesson53.py
@@ -1,5 +1,15 @@
 from tkinter import *
 
+def add_str():
+    e.insert(END, 'Hello')
+
+def del_str():
+    e.delete(0, END)
+
+def get_str():
+    l_text['text'] = e.get()
+
+
 root = Tk()
 root.title('Rainbow')
 root.iconbitmap('python.ico')
@@ -9,8 +19,15 @@ l = Label(root, text="Поле ввода")
 l.pack()
 
 e = Entry(root)
-e.insert((0), 'Hello ')
-e.insert((6), 'World')
+# e.insert(0, 'Hello')
+# e.insert(END, ' world')
 e.pack()
+
+btn_add = Button(root, text='Add', command=add_str).pack()
+btn_del = Button(root, text='Delete', command=del_str).pack()
+btn_get = Button(root, text='Get', command=get_str).pack()
+
+l_text = Label(root, bg='red', fg='white')
+l_text.pack(fill='x')
 
 root.mainloop()
