98b5479f88d004386f67e71533924c9a10887a4c
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-26 22:14:46 +0200

    lesson57

---
+++ new/lesson57.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('600x400+400+150')
+
+l1 = Label(root, text='1', font='15', fg='#fff', bg='#3498db', width=8, height=4).pack(side=BOTTOM)
+l2 = Label(root, text='2', font='15', fg='#fff', bg='#2ecc71', width=8, height=4).pack(side=BOTTOM)
+l3 = Label(root, text='3', font='15', fg='#fff', bg='#f1c40f', width=8, height=4).pack(side=BOTTOM)
+l4 = Label(root, text='4', font='15', fg='#fff', bg='#34495e', width=8, height=4).pack(side=BOTTOM)
+
+
+
+root.mainloop()
+
+
+
