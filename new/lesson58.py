31d83a041be60f6b305a63dd31b5da0e0c58c828
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-27 19:30:04 +0300

    lesson58

--- new/lesson58.py
+++ new/lesson58.py
@@ -1,4 +1,21 @@
-text = 'When \t\n you play a \t\n game of thrones you win or you die.'
+def stringToList(string):
+    listRes = list(string.split(" "))
+    return listRes
 
-# BEGIN (write your solution here)
-print(len(text[5:16].strip()))
+
+def reverse(word):
+    return word[::-1]
+
+
+def reversed_word_in_List(list):
+    newList = []
+    for i in list:
+        newList.append(reverse(i))
+    return newList
+
+
+strA = "Millie Bobby Brown"
+print(strA)
+list = stringToList(strA)
+newString = ' '.join(reversed_word_in_List(list))
+print(newString)
