df0769af387f772717f56e335a4640a1c8468d21
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-27 22:22:18 +0300

    lesson58

--- new/lesson58.py
+++ new/lesson58.py
@@ -1,21 +1,26 @@
-def stringToList(string):
-    listRes = list(string.split(" "))
-    return listRes
+from tkinter import *
+
+root = Tk()
+root.geometry('600x400+400+150')
+
+# f_top = Frame(root)
+# f_top.pack()
+# f_bottom = Frame(root)
+# f_bottom.pack()
+
+f_top = LabelFrame(root, text='TOP Frame', padx=10, pady=10)
+f_top.pack(padx=10)
+f_bottom = LabelFrame(root, text='BOTTOM Frame', padx=10, pady=10)
+f_bottom.pack()
+
+l1 = Label(f_top, text='1', font='15', fg='#fff', bg='#3498db', width=8, height=4).pack(side=LEFT)
+l2 = Label(f_top, text='2', font='15', fg='#fff', bg='#2ecc71', width=8, height=4).pack(side=LEFT)
+l3 = Label(f_bottom, text='3', font='15', fg='#fff', bg='#f1c40f', width=8, height=4).pack(side=LEFT)
+l4 = Label(f_bottom, text='4', font='15', fg='#fff', bg='#34495e', width=8, height=4).pack(side=LEFT)
 
 
-def reverse(word):
-    return word[::-1]
+
+root.mainloop()
 
 
-def reversed_word_in_List(list):
-    newList = []
-    for i in list:
-        newList.append(reverse(i))
-    return newList
 
-
-strA = "Millie Bobby Brown"
-print(strA)
-list = stringToList(strA)
-newString = ' '.join(reversed_word_in_List(list))
-print(newString)
