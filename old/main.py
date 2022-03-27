009ca4c6bdb715e368af8b4dc9bf909418c6833d
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-27 23:45:07 +0300

    lesson58

--- old/main.py
+++ old/main.py
@@ -1,3 +1,16 @@
+# import sys
+
 def hello(name='World'):
     print("Hello, {}!".format(name))
 hello('Wowa')
+
+text = 'qwerty'
+
+print(''.join(reversed(text)))
+print(text[::-1])
+print(''.join(text[::-1]))
+
+s = ''
+for i in reversed(text):
+    s += i
+print(s)
