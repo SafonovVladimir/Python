e532dc584832a6ed7ac218816a606f611784f7d9
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-28 17:40:53 +0300

    lesson58

--- old/main.py
+++ old/main.py
@@ -1,5 +1,5 @@
 # import sys
-
+#
 def hello(name='World'):
     print("Hello, {}!".format(name))
 hello('Wowa')
@@ -14,3 +14,11 @@ s = ''
 for i in reversed(text):
     s += i
 print(s)
+
+# import re
+#
+# s = "Tim's phone numbers are 12345-41512 and 78963 85214"
+# match = re.findall(r'\d{5}', s)
+#
+# if match:
+#     print(match)
