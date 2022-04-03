4a32a923f18bb88c7c16034412abba6aebc33bf9
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-04-03 16:15:30 +0300

    new kurs Lesson1

---
+++ pyWithDjango/lesson1.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+# a = 15
+# b = 11
+# c = 9
+#
+# p = (a + b + c) / 2
+# x = p * (p - a) * (p - b) * (p - c)
+# s = round(sqrt(x), 2)
+#
+
+pi = 3.14
+r = 12
+
+s = pi * r ** 2
+
+print(s)
