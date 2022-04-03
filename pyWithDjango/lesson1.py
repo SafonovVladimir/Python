7a49a5c6cca47735baf5c6e3da825be0ba42f246
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-04-03 16:32:44 +0300

    new kurs Lesson1

--- pyWithDjango/lesson1.py
+++ pyWithDjango/lesson1.py
@@ -16,8 +16,23 @@ from math import sqrt
 #
 # print(s)
 
-for i in range(1, 11):
-    if i % 2 == 0:
-        print(f'{i} число четное')
-    else:
-        print(f'{i} число нечетное')
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         print(f'{i} число четное')
+#     else:
+#         print(f'{i} число нечетное')
+
+# name = 'Vladimir Safonov'
+# list1 = list(name.split(' '))
+# print(list1)
+# list = []
+#
+# for i in name:
+#     list.append(i)
+
+# print(list)
+
+some_list = [('qwe', 22), ('asd', 44), ('zxc', 66)]
+
+for (name, age) in some_list:
+    print(f'{name} is {age} years old')
