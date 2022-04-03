6b8df786356e10f8a70d49da67920e625f7ed314
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-04-03 16:23:17 +0300

    new kurs Lesson1

--- pyWithDjango/lesson1.py
+++ pyWithDjango/lesson1.py
@@ -8,10 +8,16 @@ from math import sqrt
 # x = p * (p - a) * (p - b) * (p - c)
 # s = round(sqrt(x), 2)
 #
+#
+# pi = 3.14
+# r = 12
+#
+# s = pi * r ** 2
+#
+# print(s)
 
-pi = 3.14
-r = 12
-
-s = pi * r ** 2
-
-print(s)
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f'{i} число четное')
+    else:
+        print(f'{i} число нечетное')
